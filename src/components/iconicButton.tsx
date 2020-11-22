73f517f629629ab26ea692dbdd29921e66d7c667
benhakimilyass <ilyassbenhakim2@gmail.com>
2020-11-22 10:07:53 +0100

    shore: adding links

--- src/components/iconicButton.tsx
+++ src/components/iconicButton.tsx
@@ -2,21 +2,36 @@ import styled from "@xstyled/styled-components";
 import { Hstack } from "@components";
 import { motion } from "framer-motion";
 
-const IconicButton = ({ title, bgcolor = "#000", color = "#fff", children, isCentered = false, style = {} }) => {
+const IconicButton = ({
+  title,
+  bgcolor = "#000",
+  color = "#fff",
+  children,
+  isCentered = false,
+  style = {},
+  ...props
+}) => {
   return (
-    <Thumbnail style={{ backgroundColor: bgcolor, color, ...style }} whileHover={{ y: -8 }}>
-      <Hstack alignItems="center" space={1} justifyContent={isCentered ? "center" : "flex-start"}>
-        <P>{title}</P>
-        {children}
-      </Hstack>
-    </Thumbnail>
+    <a {...props}>
+      <Thumbnail style={{ backgroundColor: bgcolor, color, ...style }} whileHover={{ y: -6 }}>
+        <Hstack alignItems="center" space={1} justifyContent={isCentered ? "center" : "flex-start"}>
+          <P>{title}</P>
+          {children}
+        </Hstack>
+      </Thumbnail>
+    </a>
   );
 };
 const Thumbnail = styled(motion.button)`
   height: 44px;
+  width: 100%;
   border: none;
   padding: 8px 16px;
   cursor: pointer;
+  a {
+    text-decoration: none;
+    color: white !important;
+  }
 `;
 const P = styled.p`
   font-size: 17px;
